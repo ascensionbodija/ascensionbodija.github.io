1bbf46fd247394069594d4a1f41c291df17f2b56
ascensionbodija <116366249+ascensionbodija@users.noreply.github.com>
2022-10-21 18:06:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function menuBar(){this.classList.toggle('flip')};
+function menuBar(){
+    this.classList.toggle('flip');
+    all.classList.toggle('vis');
+    sidemenu.classList.toggle('vie');
+};
 let ham = document.querySelector('#hamburger');
+let all = document.querySelector('.all');
+let sidemenu = document.querySelector('.sidemenu');
 ham.addEventListener('click',menuBar);
