f37e6467b55d79e4ae479f32f9a96de23deb128b
ascensionbodija <116366249+ascensionbodija@users.noreply.github.com>
2022-10-22 14:39:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+/*the site as a whole has been written using vanilla JavaScript (ES6) and CSS3 with a strong emphasis on 
+quick, easy, long-term website maintenance in mind*/
+/*when adding new features please refrain from using frameworks like bootstrap*/
+/*or the reactJS library, unless absolutely neccesary.*/
+/*the code is straightforward enough if you know what you are doing*/
+
+alert('Site under construction! Status quo for preview purposes only!')
 function menuBar(){
     this.classList.toggle('flip');
+    all.style.transition="1s";
     all.classList.toggle('vis');
     sidemenu.classList.toggle('vie');
 };
